b0ab1613d48c4540711f5ec90d04d83572d603e3
craigh6247 <craig.hollington@googlemail.com>
2024-06-05 18:44:05 +0100

    update blog title

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -43,7 +43,7 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Launcg Blog"
+      title="Launch Blog"
       intro="Welcome to chollington.com the inorgral blog post, product launches, cloud secruity, etc ">
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
